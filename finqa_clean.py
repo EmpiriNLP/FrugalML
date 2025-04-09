38cd6fbd66d3ccc02474d9a50003738a22a59ac2
Gnefil <lifeng.lin24@ucl.ac.uk>
2025-04-09 14:56:53 +0100

    Clean empty answers finqa and prepare for test results

--- finqa_clean.py
+++ finqa_clean.py
@@ -5,15 +5,23 @@ import os
 load_dotenv()
 DATSET_DIR = os.getenv("DATASET_DIR") + "FinQA/dataset/"
 
-data_path = DATSET_DIR + "train.json"
-to_path = DATSET_DIR + "train.cleaned.json"
+sets = ["train", "dev", "test"]
+for set in sets:
+    data_path = DATSET_DIR + f"{set}.json"
+    to_path = DATSET_DIR + f"{set}.cleaned.json"
 
-with open(data_path, "r") as f:
-    data = json.load(f)
+    with open(data_path, "r") as f:
+        data = json.load(f)
 
-for element in data:
-    element["qa"]["exe_ans"] = str(element["qa"]["exe_ans"])
+    print(f"Original {set} data size: {len(data)}")
+    for element in data:
+        if element["qa"]["answer"] == "":
+            data.remove(element)
+        else:
+            element["qa"]["exe_ans"] = str(element["qa"]["exe_ans"])
 
-with open(to_path, "w") as f:
-    json.dump(data, f, indent=4)
+    print(f"Cleaned {set} data size: {len(data)}")
+
+    with open(to_path, "w") as f:
+        json.dump(data, f, indent=4)
 
